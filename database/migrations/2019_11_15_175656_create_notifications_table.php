f78379a4129de85f037427483ef9bde55e4cc65d
RamiSalm <ramisalmaljaf@gmail.com>
2019-11-15 21:10:39 +0300

    Add Notification Table

---
+++ database/migrations/2019_11_15_175656_create_notifications_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 100);
+            $table->text('body');
+            $table->string('notfiImg', 250);
+            $table->bigInteger('school_id')->unsigned();
+            // Foregin key
+            $table->foreign('school_id')->references('id')->on('schools');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
